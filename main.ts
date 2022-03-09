084b0808dfa6ce62f03c8840e1b9519ad770e371
heycalmdown <heyacct@gmail.com>
2022-03-09 17:45:55 +0900

    feat: 20 positions only for 1 file

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -13,69 +13,69 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
+	backward: number[] = [];
+	forward: number[] = [];
+	cur = 0;
+
+	restoreState() {
+		this.backward = []
+		this.forward = []
+		this.cur = 0
+	}
+
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerEvent(
+			this.app.workspace.on('file-open', (file) => this.restoreState()),
+		);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'cursor-position-backward',
+			name: 'Go back',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				if (this.backward.length < 1) return;
+				const prev = this.backward.pop()
+				this.forward.push(this.cur)
+				this.cur = prev
+				editor.setSelection({ line: prev, ch: 0 });
+				console.log(this.backward, this.cur, this.forward)
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'cursor-position-forward',
+			name: 'Go forward',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				if (this.forward.length < 1) return;
+				const prev = this.forward.pop()
+				this.backward.push(this.cur)
+				this.cur = prev
+				editor.setSelection({ line: prev, ch: 0 });
+				console.log(this.backward, this.cur, this.forward)
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		this.registerInterval(window.setInterval(() => {
+			// const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+			const editor = this.app.workspace.getActiveViewOfType(MarkdownView)?.editor
+			if (editor) {
+				// const from = editor.getCursor("anchor");
+				const to = editor.getCursor("head");
+				if (to.line === this.cur) return;
+				this.backward.push(this.cur)
+				this.cur = to.line
+				this.backward = this.backward.slice(-20)
+				this.forward = []
+				console.log(this.backward, this.cur, this.forward)
+			}
+		}, 1 * 1000));
 	}
 
 	onunload() {
@@ -90,23 +90,6 @@ export default class MyPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 }
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
